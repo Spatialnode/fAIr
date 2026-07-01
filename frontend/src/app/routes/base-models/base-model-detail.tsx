840f59985425a5157864d84400a36857e1d5b108
shittu adewale <sheriffhardewale@gmail.com>
2026-07-01 09:19:21 +0100

    chore: rearranged detail items for mobile

--- frontend/src/app/routes/base-models/base-model-detail.tsx
+++ frontend/src/app/routes/base-models/base-model-detail.tsx
@@ -361,10 +361,9 @@ export const BaseModelDetailPage = () => {
         {/* Main Content: Two Column Layout */}
         <div className="grid grid-cols-1 lg:grid-cols-[minmax(0,_1fr)_600px] gap-x-12 gap-y-10">
           {/* Left Column - Overview */}
-        <div className="order-2 lg:order-1">
-
-          <MarkdownViewer content={model.markdownContent} />
-        </div>
+          <div className="order-2 lg:order-1">
+            <MarkdownViewer content={model.markdownContent} />
+          </div>
 
           {/* Right Column - Architecture Info */}
           <div className="flex order-1 lg:order-2 gap-4 flex-col hide-scrollbar lg:sticky lg:top-8 lg:self-start lg:max-h-[calc(100vh-4rem)] lg:overflow-y-auto">
